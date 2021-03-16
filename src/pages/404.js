3a39ef2a13e524e88a0e2b148ea8576e4b6c2a9d
samueljmillward <samuel.j.pearce@hotmail.com>
2021-03-16 16:26:38 +0000

    Update 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+import StyledHeader from '../components/StyledHeader';
+
+export default () => (
+    <Layout>
+        <StyledHeader>Oops!</StyledHeader>
+        <p>There's nothing here!</p>
+    </Layout>
+);
