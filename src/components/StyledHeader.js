8e712cc81d307b51f655e15cf589d04fc9fb4414
samueljmillward <samuel.j.pearce@hotmail.com>
2021-03-16 16:14:55 +0000

    Add 404 page

--- src/components/StyledHeader.js
+++ src/components/StyledHeader.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const StyledHeader = styled.h1`
     display: block;
     color: white;
-    margin-top: 16rem;
+    margin-top: 14rem;
 
     @media (max-width: 600px) {
         margin-top: 10rem;
